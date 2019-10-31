576dac2796b6bc3e5d78c3928c5ac66981f7ad3e
Shivam Jaiswal <shivam.ducs@gmail.com>
2019-10-31 11:09:52 +0530

    initial setup

---
+++ Lalamove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lalamove
+//
+//  Created by Shivam Jaiswal on 31/10/19.
+//  Copyright © 2019 Shivam Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
